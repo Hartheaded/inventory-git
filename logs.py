1a417a1bd9cebe0e81b2ab38c4d3b1eeab21a45a
Mason Hart <hartmasonj@gmail.com>
2024-02-06 09:18:35 -0600

    Added more files for future use, tweaked login.py Account.login output to always bee the name of the user

--- logs.py
+++ logs.py
@@ -1,7 +1,14 @@
+import re
+inventory = open("accounts/inventory.log", "w+")
+print(inventory.read())
 class Logs():
 
 
     def __init__(self):
+        pass
 
     def __new__(cls):
+        pass
 
+    def newEntry(self):
+        pass
